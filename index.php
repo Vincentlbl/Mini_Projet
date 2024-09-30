d1871d85182ff9f1b2b6a605bf04f771aae8447f
Vincent <vlebel94@gmail.com>
2024-09-30 18:14:38 +0200

    Ajout de la page d'accueil avec Index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil - Livre d'or</title>
+</head>
+<body>
+    <h1>Bienvenue dans notre Livre d'or</h1>
+    <p>Ce livre d'or vous permet de laisser des messages après vous être inscrit et connecté. Vous pouvez consulter les messages laissés par d'autres utilisateurs.</p>
+    <p><a href="login_signup.php ">Accéder à l'espace membre</a> pour vous connecter ou vous inscrire.</p>
+</body>
+</html>
